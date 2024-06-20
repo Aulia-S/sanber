952fdd9440ae0af7b2312f3cb7b0339bb0a65843
Aulia Sabri <auliasabri43@gmail.com>
2024-06-20 14:36:13 +0700

    tugas 8

---
+++ Tugas-8/src/utils/env.ts
@@ -0,0 +1,4 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || ''
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || ''
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || ''
+export const DATABASE_URL: string = 'mongodb+srv://ecommerce:ecommerce@ecommerce.u7yhg7x.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce'
