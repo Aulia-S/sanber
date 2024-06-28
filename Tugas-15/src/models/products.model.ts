11de5ff864b3eb922d0be36435b639a12d27e1f0
Aulia Sabri <auliasabri43@gmail.com>
2024-06-28 19:46:49 +0700

    tugas 15

---
+++ Tugas-15/src/models/products.model.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const ProductsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    qty: {
+      type: Number,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Category',
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+ProductsSchema.pre('save', function (next) {
+  const product = this
+
+  if (!product.slug) {
+    product.slug = product.name.toLowerCase().split(' ').join('-')
+  }
+
+  next()
+})
+
+const ProductsModel = mongoose.model('Product', ProductsSchema)
+
+export default ProductsModel
